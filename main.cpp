7143bbcbe1add9e017d350ace2aa6da753d249f4
originalname51 <originalname51@users.noreply.github.com>
2016-09-28 20:03:48 -0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "main.hpp"
+
+
+int main() {
+    std::cout << hello() << std::endl;
+    return 0;
+}
+
+std::string hello(){
+	return "Hello there world";
+}
---
+++ main.hpp
@@ -0,0 +1,17 @@
+/*
+ * main.hpp
+ *
+ *  Created on: Sep 28, 2016
+ *      Author: rob
+ */
+#include <iostream>
+
+#ifndef MAIN_HPP_
+#define MAIN_HPP_
+
+std::string hello();
+
+
+
+
+#endif /* MAIN_HPP_ */
